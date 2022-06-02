28b1fa9910b0c6e7cb9bd55d1cd5f89f1c74bba1
gredajustyna <83274413+gredajustyna@users.noreply.github.com>
2022-06-02 11:31:50 +0200

    added RSA signer exception

--- lib/rsa_signer.dart
+++ lib/rsa_signer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/services.dart';
 
+import 'exceptions.dart';
+
 class RSASigner{
   static const MethodChannel _channel = MethodChannel('thclab_signing_plugin');
   String uuid;
@@ -10,29 +12,54 @@ class RSASigner{
 
   ///Returns currently used public key for RSA signer.
   Future<String> getCurrentPubKey() async{
-    var key = await _channel.invokeMethod("getRSAKey", {'alias': "${uuid}_0_rsa"});
-    return key;
+    var isCorrectUuid = await _channel.invokeMethod('checkUuid', {'uuid' : uuid});
+    if(isCorrectUuid){
+      var key = await _channel.invokeMethod("getRSAKey", {'alias': "${uuid}_0_rsa"});
+      return key;
+    }else{
+      throw IncorrectUuidException('There are no keys associated with this UUID saved on the device');
+    }
   }
 
   ///Returns second/backup public key for RSA signer.
   Future<String> getNextPubKey() async{
-    var key = await _channel.invokeMethod("getRSAKey", {'alias': "${uuid}_1_rsa"});
-    return key;
+    var isCorrectUuid = await _channel.invokeMethod('checkUuid', {'uuid' : uuid});
+    if(isCorrectUuid){
+      var key = await _channel.invokeMethod("getRSAKey", {'alias': "${uuid}_1_rsa"});
+      return key;
+    }else{
+      throw IncorrectUuidException('There are no keys associated with this UUID saved on the device');
+    }
   }
 
   ///Returns the uuid of RSA signer.
-  String getUuid(){
-    return uuid;
+  Future<String> getUuid() async{
+    var isCorrectUuid = await _channel.invokeMethod('checkUuid', {'uuid' : uuid});
+    if(isCorrectUuid){
+      return uuid;
+    }else{
+      throw IncorrectUuidException('There are no keys associated with this UUID saved on the device');
+    }
   }
 
   ///Rotates the keys (the backup key becomes currently used and new key is generated as backup).
   Future<void> rotateForRSA() async{
-    await _channel.invokeMethod("rotateForRSA", {'uuid' : uuid});
+    var isCorrectUuid = await _channel.invokeMethod('checkUuid', {'uuid' : uuid});
+    if(isCorrectUuid){
+      await _channel.invokeMethod("rotateForRSA", {'uuid' : uuid});
+    }else{
+      throw IncorrectUuidException('There are no keys associated with this UUID saved on the device');
+    }
   }
 
   ///Signs provided message using RSA. Returns signature if successfully signed.
   Future<String> sign(String message) async{
-    var signature = await _channel.invokeMethod("signRSA", {'uuid' : uuid, 'message' : message});
-    return signature;
+    var isCorrectUuid = await _channel.invokeMethod('checkUuid', {'uuid' : uuid});
+    if(isCorrectUuid){
+      var signature = await _channel.invokeMethod("signRSA", {'uuid' : uuid, 'message' : message});
+      return signature;
+    }else{
+      throw IncorrectUuidException('There are no keys associated with this UUID saved on the device');
+    }
   }
 }
